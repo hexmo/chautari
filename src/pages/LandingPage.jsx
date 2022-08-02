f3b3581c24eb55aec4b6bab16a9c2229c69a57a2
Mohit Acharya <35497915+hexmo@users.noreply.github.com>
2022-08-02 22:19:54 +0545

    feat: add google sign in

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,4 +1,28 @@
+import {
+  signInWithRedirect,
+  getRedirectResult,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
+import { auth } from "../firebase";
+import google from "../assets/images/landing-page/google-signin.png";
+
+const provider = new GoogleAuthProvider();
+
+getRedirectResult(auth)
+  .then((result) => {
+    console.log(result.user);
+  })
+  .catch((error) => {
+    // Handle Errors here.
+    console.log(error);
+  });
+
 function LandingPage() {
+  function signInWithProvider() {
+    signInWithRedirect(auth, provider);
+  }
+
   return (
     <div className="h-screen flex flex-row">
       <div className="lg:basis-2/5 w-full text-center">
@@ -11,6 +35,16 @@ function LandingPage() {
             Create the perfect memories with your loved ones with high quality
             uninterrupted live video streams.
           </p>
+
+          <div className="grid justify-center">
+            <img
+              role="button"
+              src={google}
+              onClick={signInWithProvider}
+              alt="Sign in with Google"
+              className="h-9 object-contain hover:shadow-2xl hover:saturate-200"
+            />
+          </div>
         </div>
       </div>
       <div className="lg:block lg:basis-3/5 bg-chautari-image bg-bottom bg-contain bg-no-repeat opacity-20 lg:opacity-95 h-screen w-screen lg:static fixed -z-10"></div>
